f8c0e18468f4d04c89f4a40567a330901288592f
Unknown <rodrigobazan12@gmail.com>
2018-09-06 15:09:35 -0300

    [Sermed-Data] Listo para realizar los test de integracion. Agregamos application.properties para los test

--- Sermed-Data/src/test/java/ar/com/koodi/sermeddata/IntegrationTest/EnferemeroRepositorioIntegrationTest.java
+++ Sermed-Data/src/test/java/ar/com/koodi/sermeddata/IntegrationTest/EnferemeroRepositorioIntegrationTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-
 public class EnferemeroRepositorioIntegrationTest {
 
     @Autowired
