3b8f598eecd1ceaed3467327a9438bc2adf6421a
Unknown <rvegasaavedra@gmail.com>
2018-09-17 13:07:09 -0300

    [Sermed-Core] Test de consultar Afeccion- Listo!

---
+++ Sermed-Core/src/main/java/Interactor/ConsultarAfeccionUseCase.java
@@ -0,0 +1,37 @@
+package Interactor;
+
+import static org.mockito.Mockito.after;
+
+import java.util.List;
+
+import Excepciones.AfeccionNoExisteException;
+import Modelo.Afeccion;
+import Repositorio.IAfeccionRepositorio;
+
+public class ConsultarAfeccionUseCase {
+	
+	private IAfeccionRepositorio repositorioAfeccion;
+
+	public ConsultarAfeccionUseCase(IAfeccionRepositorio repositorioAfeccion) {
+		this.repositorioAfeccion = repositorioAfeccion;
+	}
+
+	public List<Afeccion> consultarAfecciones() {
+		return this.repositorioAfeccion.findAll();
+	}
+
+	public List<Afeccion> consultarAfeccionesPorNombre(String afeccion) {
+		return this.repositorioAfeccion.findByNombre(afeccion);
+	}
+
+	public Afeccion consultarAfeccionPorNombre(String afeccion) throws AfeccionNoExisteException {
+		Afeccion resultado = this.repositorioAfeccion.findByNombreUnico(afeccion);
+		if(resultado !=null) {
+			return resultado;
+		}
+		throw new AfeccionNoExisteException();
+	}
+	
+	
+
+}
