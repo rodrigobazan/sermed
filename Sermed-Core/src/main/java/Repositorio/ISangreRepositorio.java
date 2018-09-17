76d474fddf651e24e92e7dd2cd1ef91a49406c31
german Torres <gtorres@undec.edu.ar>
2018-09-17 16:03:19 -0300

    Consultar Tipo Sangre

---
+++ Sermed-Core/src/main/java/Repositorio/ISangreRepositorio.java
@@ -0,0 +1,16 @@
+package Repositorio;
+
+import Modelo.Sangre;
+
+import java.util.Collection;
+
+public interface ISangreRepositorio {
+
+    Collection<Sangre> findAll();
+
+    Collection<Sangre> findByGrupo(String grupo);
+
+    Collection<Sangre> findByFactor(String factor);
+
+    Collection<Sangre> findByGrupoFactor(String grupo, String factor);
+}
