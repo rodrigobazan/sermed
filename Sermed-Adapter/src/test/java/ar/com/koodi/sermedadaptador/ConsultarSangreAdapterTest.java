83c1521fc1359329cf9ffc3bb22caa1f6540658e
german Torres <gtorres@undec.edu.ar>
2018-10-11 15:01:59 -0300

    []Sermed - adapter] agrerga test a sangre

--- Sermed-Adapter/src/test/java/ar/com/koodi/sermedadaptador/ConsultarSangreAdapterTest.java
+++ Sermed-Adapter/src/test/java/ar/com/koodi/sermedadaptador/ConsultarSangreAdapterTest.java
@@ -72,6 +72,15 @@ public class ConsultarSangreAdapterTest {
         assertThat(sangreDTOS, not(IsEmptyCollection.empty()));
     }
 
+    @Test
+    public void consultarSangrePorFactorGrupo_CriterioConDatos_DevuelveSangre() {
+        when(consultarSangreInput.consultarSangrePorGrupoFactor("A","RH+")).thenReturn(new Sangre(1,"A","RH+"));
+        ConsultarSangreAdapter consultarSangreAdapter = new ConsultarSangreAdapter(consultarSangreInput);
+        SangreDTO sangreDTO = consultarSangreAdapter.consultarSangrePorGrupoFactor("A","RH+");
+        Assertions.assertEquals("A");
+
+    }
+
 
 
 
