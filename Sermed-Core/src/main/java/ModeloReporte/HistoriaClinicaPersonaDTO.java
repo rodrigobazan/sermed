16281b6d681c3c7fbd6cbff0f85676dd389c82b5
Unknown <rvegasaavedra@gmail.com>
2018-10-16 12:26:14 -0300

    [Sermed-Adapter] Test GenerarHistoriaClinicaPersona

--- Sermed-Core/src/main/java/ModeloReporte/HistoriaClinicaPersonaDTO.java
+++ Sermed-Core/src/main/java/ModeloReporte/HistoriaClinicaPersonaDTO.java
@@ -9,9 +9,26 @@ import java.util.List;
 public class HistoriaClinicaPersonaDTO {
     private Persona personaBuscada;
     private List<Visita> visitasDeLaPersona;
+    private List<AntecedenteMedico> antecedenteMedicos;
 
     public HistoriaClinicaPersonaDTO(Persona personaBuscada, List<AntecedenteMedico> antecedenteMedicos, List<Visita> visitasDeLaPersona) {
         this.personaBuscada = personaBuscada;
         this.visitasDeLaPersona = visitasDeLaPersona;
+        this.antecedenteMedicos = antecedenteMedicos;
     }
+
+	public Persona getPersonaBuscada() {
+		return personaBuscada;
+	}
+	
+	public List<Visita> getVisitasDeLaPersona() {
+		return visitasDeLaPersona;
+	}
+
+	public List<AntecedenteMedico> getAntecedenteMedicos() {
+		return antecedenteMedicos;
+	}
+
+	
+	    
 }
