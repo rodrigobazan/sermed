0df8c6c53bca1688addbf87cea3f1181eddef32b
Unknown <rodrigobazan12@gmail.com>
2018-10-17 12:48:44 -0300

    [Sermed-Service] Creacion del proyecto

---
+++ Sermed-Services/src/main/java/ar/com/koodi/sermedservices/SermedServicesApplication.java
@@ -0,0 +1,12 @@
+package ar.com.koodi.sermedservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SermedServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SermedServicesApplication.class, args);
+    }
+}
