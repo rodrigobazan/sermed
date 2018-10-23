075804b4a076ea2395ac98039ed83bfcd5aebb45
Rodrigo <rabazan@undec.edu.ar>
2018-10-23 15:18:20 -0300

    [Sermed-Services] Test crear enfermero listo

--- Sermed-Boundaries/src/main/java/ar/com/koodi/sermedboundaries/SermedServices/Controller/CrearObraSocialController.java
+++ Sermed-Boundaries/src/main/java/ar/com/koodi/sermedboundaries/SermedServices/Controller/CrearObraSocialController.java
@@ -4,7 +4,6 @@ import Adaptadores.CrearObraSocialAdapter;
 import Excepciones.ObraSocialExisteException;
 import ModeloApi.ObraSocialDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
---
+++ Sermed-Boundaries/src/test/java/ar/com/koodi/sermedboundaries/ServiceIntegrationTest/CrearEnfermeroIntegrationTest.java
@@ -0,0 +1,66 @@
+package ar.com.koodi.sermedboundaries.ServiceIntegrationTest;
+
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CrearEnfermeroIntegrationTest {
+
+    private String url = "http://localhost:8080";
+
+    @Test
+    public void crearEnfermero_GuardaCorrectamente_Deuelve200() throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("idEnfermero", null);
+        jsonObject.put("apellido", "Ruitti");
+        jsonObject.put("nombre", "Javier");
+        jsonObject.put("matricula", 190202);
+        jsonObject.put("telefono", "123456789");
+        String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+        Header header = new BasicHeader("Authorization", "Bearer "+token);
+        HttpPost post = new HttpPost(url+"/sermed/enfermero/nuevo");
+        StringEntity se = new StringEntity(jsonObject.toString());
+        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+        post.setEntity(se);
+        post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpUriRequest request = post;
+        request.setHeader(header);
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+    }
+
+    @Test
+    public void crearEnfermero_EnfermeroYaExiste_Deuelve412() throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("idEnfermero", null);
+        jsonObject.put("apellido", "Ruitti");
+        jsonObject.put("nombre", "Javier");
+        jsonObject.put("matricula", 190202);
+        jsonObject.put("telefono", "123456789");
+        String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+        Header header = new BasicHeader("Authorization", "Bearer "+token);
+        HttpPost post = new HttpPost(url+"/sermed/enfermero/nuevo");
+        StringEntity se = new StringEntity(jsonObject.toString());
+        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+        post.setEntity(se);
+        post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpUriRequest request = post;
+        request.setHeader(header);
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_PRECONDITION_FAILED));
+    }
+
+}
