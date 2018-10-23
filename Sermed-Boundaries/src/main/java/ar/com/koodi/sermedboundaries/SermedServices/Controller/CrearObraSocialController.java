a89a0522ad242eaa08ee03b808a436c81e5fdb82
Rodrigo <rabazan@undec.edu.ar>
2018-10-23 14:54:14 -0300

    [Sermed-Service] Test crear obra social listo

---
+++ Sermed-Boundaries/src/main/java/ar/com/koodi/sermedboundaries/SermedServices/Controller/CrearObraSocialController.java
@@ -0,0 +1,34 @@
+package ar.com.koodi.sermedboundaries.SermedServices.Controller;
+
+import Adaptadores.CrearObraSocialAdapter;
+import Excepciones.ObraSocialExisteException;
+import ModeloApi.ObraSocialDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/sermed/obraSocial")
+public class CrearObraSocialController {
+
+    private final CrearObraSocialAdapter crearObraSocialAdapter;
+
+    public CrearObraSocialController(CrearObraSocialAdapter crearObraSocialAdapter) {
+        this.crearObraSocialAdapter = crearObraSocialAdapter;
+    }
+
+    @RequestMapping(value = "/nueva", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> crearObraSocial(@RequestBody @Valid ObraSocialDTO obraSocial) {
+        try {
+            boolean resultado = crearObraSocialAdapter.crearObraSocial(obraSocial);
+            if (resultado) return ResponseEntity.status(HttpStatus.OK).body(true);
+            else return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (ObraSocialExisteException e) {
+            return new ResponseEntity<>(HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+}
