29b871c7f6000bdb771b4beaf75029fc2aead3e9
Rodrigo <rabazan@undec.edu.ar>
2018-10-23 15:38:05 -0300

    [Sermed-Service] Test de crear medico listo

--- Sermed-Boundaries/src/test/java/ar/com/koodi/sermedboundaries/ServiceIntegrationTest/CrearMedicoIntegrationTest.java
+++ Sermed-Boundaries/src/test/java/ar/com/koodi/sermedboundaries/ServiceIntegrationTest/CrearMedicoIntegrationTest.java
@@ -10,28 +10,27 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HTTP;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class CrearEnfermeroIntegrationTest {
+public class CrearMedicoIntegrationTest {
 
     private String url = "http://localhost:8080";
 
     @Test
-    public void crearEnfermero_EnfermeroNoExiste_Deuelve200() throws Exception {
+    public void crearMedico_MedicoNoExiste_Devuelve200() throws Exception {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("idEnfermero", null);
-        jsonObject.put("apellido", "Ruitti");
-        jsonObject.put("nombre", "Javier");
-        jsonObject.put("matricula", 190202);
+        jsonObject.put("idMedico", null);
+        jsonObject.put("apellido", "Vega");
+        jsonObject.put("nombre", "Romina");
+        jsonObject.put("matricula", 192035);
         jsonObject.put("telefono", "123456789");
         String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
         Header header = new BasicHeader("Authorization", "Bearer "+token);
-        HttpPost post = new HttpPost(url+"/sermed/enfermero/nuevo");
+        HttpPost post = new HttpPost(url+"/sermed/medico/nuevo");
         StringEntity se = new StringEntity(jsonObject.toString());
         se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
         post.setEntity(se);
@@ -43,16 +42,16 @@ public class CrearEnfermeroIntegrationTest {
     }
 
     @Test
-    public void crearEnfermero_EnfermeroYaExiste_Deuelve412() throws Exception {
+    public void crearMedico_MedicoExiste_Devuelve412() throws Exception {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("idEnfermero", null);
-        jsonObject.put("apellido", "Ruitti");
-        jsonObject.put("nombre", "Javier");
-        jsonObject.put("matricula", 190202);
+        jsonObject.put("idMedico", null);
+        jsonObject.put("apellido", "Vega");
+        jsonObject.put("nombre", "Romina");
+        jsonObject.put("matricula", 192035);
         jsonObject.put("telefono", "123456789");
         String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
         Header header = new BasicHeader("Authorization", "Bearer "+token);
-        HttpPost post = new HttpPost(url+"/sermed/enfermero/nuevo");
+        HttpPost post = new HttpPost(url+"/sermed/medico/nuevo");
         StringEntity se = new StringEntity(jsonObject.toString());
         se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
         post.setEntity(se);
@@ -62,5 +61,4 @@ public class CrearEnfermeroIntegrationTest {
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
         assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_PRECONDITION_FAILED));
     }
-
 }
