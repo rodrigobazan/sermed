3862b09fbe022c26cf145baf7eda17cfdac4af40
Rodrigo <rabazan@undec.edu.ar>
2018-10-24 15:57:07 -0300

    [Sermed-Services] Test Modificar obra social listo

---
+++ Sermed-Boundaries/src/test/java/ar/com/koodi/sermedboundaries/ServiceIntegrationTest/ModificarObraSocialIntegrationTest.java
@@ -0,0 +1,64 @@
+package ar.com.koodi.sermedboundaries.ServiceIntegrationTest;
+
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ModificarObraSocialIntegrationTest {
+
+    private String url = "http://localhost:8080";
+
+    @Test
+    public void modificarObraSocial_DatosCorrectos_Devuelve200() throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("idObraSocial", 12);
+        jsonObject.put("obraSocial", "OSDE");
+        String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+        Header header = new BasicHeader("Authorization", "Bearer "+token);
+        HttpPost post = new HttpPost(url+"/sermed/obraSocial/modificar");
+        StringEntity se = new StringEntity(jsonObject.toString());
+        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+        post.setEntity(se);
+        post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpUriRequest request = post;
+        request.setHeader(header);
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+    }
+
+    @Test
+    public void modificarObraSocial_ObraSocialYaExiste_Devuelve412() throws Exception {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("idObraSocial", 13);
+        jsonObject.put("obraSocial", "OSDE");
+        String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+        Header header = new BasicHeader("Authorization", "Bearer "+token);
+        HttpPost post = new HttpPost(url+"/sermed/obraSocial/modificar");
+        StringEntity se = new StringEntity(jsonObject.toString());
+        se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+        post.setEntity(se);
+        post.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpUriRequest request = post;
+        request.setHeader(header);
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_PRECONDITION_FAILED));
+        Assertions.assertEquals("Ya existe esta obra social", EntityUtils.toString(httpResponse.getEntity()));
+    }
+
+
+
+}
