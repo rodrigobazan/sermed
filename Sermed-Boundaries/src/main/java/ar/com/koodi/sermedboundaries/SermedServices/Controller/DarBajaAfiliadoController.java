95c9f9a0c91d05d178f369d4666830e5117eac2d
Rodrigo <rabazan@undec.edu.ar>
2018-10-31 11:24:46 -0300

    [Sermed-Services] dar baja afiliado test listo

---
+++ Sermed-Boundaries/src/main/java/ar/com/koodi/sermedboundaries/SermedServices/Controller/DarBajaAfiliadoController.java
@@ -0,0 +1,45 @@
+package ar.com.koodi.sermedboundaries.SermedServices.Controller;
+
+import Adaptadores.DarBajaAfiliadoAdapter;
+import Excepciones.*;
+import ModeloApi.AfiliadoDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/sermed/afiliado")
+public class DarBajaAfiliadoController {
+
+    private DarBajaAfiliadoAdapter darBajaAfiliadoAdapter;
+
+    public DarBajaAfiliadoController(DarBajaAfiliadoAdapter darBajaAfiliadoAdapter) {
+        this.darBajaAfiliadoAdapter = darBajaAfiliadoAdapter;
+    }
+
+    @RequestMapping(value = "/darBaja/{fecha}", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<?> darBajaAfiliado(@RequestBody @Valid AfiliadoDTO afiliado, @PathVariable("fecha")String fecha){
+        try {
+            LocalDate fechaParseada = LocalDate.parse(fecha);
+            return ResponseEntity.status(HttpStatus.OK).body(darBajaAfiliadoAdapter.darBajaAfiliado(afiliado, fechaParseada));
+        } catch (PersonaIncompletaException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Faltan datos de personas");
+        } catch (PlanIncompletoException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Faltan datos de plan");
+        } catch (AfiliadoSinPlanException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Afiliado sin plan");
+        } catch (NumeroAfiliadoIncorrectoException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Numero de afiliado incorrecto");
+        } catch (AfiliadoSinTitularException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Afiliado sin titular");
+        } catch (DniConPuntosException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Formato dni incorrecto");
+        } catch (AfiliadoDeBajaException e) {
+            return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Afiliado ya inactivo");
+        }
+    }
+}
