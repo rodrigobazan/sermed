a5fcbc83b4ea832fb9c942d48d6c7f566002dd6b
Romina <rvegasaavedra@gmail.com>
2018-10-31 12:32:42 -0300

    [Sermed-Services] Test ConsultarComprobanteDeAfiliado

---
+++ Sermed-Boundaries/src/main/java/ar/com/koodi/sermedboundaries/SermedServices/Controller/ConsultarComprobantDeAfiliadoController.java
@@ -0,0 +1,91 @@
+package ar.com.koodi.sermedboundaries.SermedServices.Controller;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import Adaptadores.ConsultarAfiliadoAdapter;
+import Adaptadores.ConsultarComprobanteDeAfiliadoAdapter;
+import Excepciones.AfiliadoNoExisteException;
+import Excepciones.AfiliadoSinPlanException;
+import Excepciones.AfiliadoSinTitularException;
+import Excepciones.DniConPuntosException;
+import Excepciones.FechaIncorrectaException;
+import Excepciones.NumeroAfiliadoIncorrectoException;
+import Excepciones.PersonaIncompletaException;
+import Excepciones.PlanIncompletoException;
+import ModeloApi.AfiliadoDTO;
+import ModeloApi.ComprobanteDTO;
+
+@RestController
+@RequestMapping("/sermed/")
+public class ConsultarComprobantDeAfiliadoController {
+
+	private ConsultarComprobanteDeAfiliadoAdapter consultarComprobanteDeAfiliadoAdapter;
+	
+	private ConsultarAfiliadoAdapter consultarAfiliadoAdapter;
+
+	public ConsultarComprobantDeAfiliadoController(
+			ConsultarComprobanteDeAfiliadoAdapter consultarComprobanteDeAfiliadoAdapter,
+			ConsultarAfiliadoAdapter consultarAfiliadoAdapter) {
+		super();
+		this.consultarComprobanteDeAfiliadoAdapter = consultarComprobanteDeAfiliadoAdapter;
+		this.consultarAfiliadoAdapter = consultarAfiliadoAdapter;
+	}
+	
+	@RequestMapping(value = "comprobantes/afiliado/numeroafiliado/{nroAfiliado}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<?> consultarTodosLosComprobantes(@PathVariable("nroAfiliado")String nroAfiliado){
+		try {
+		AfiliadoDTO afiliadoDTO = consultarAfiliadoAdapter.consultarAfiliadoPorNumero(nroAfiliado);
+		Collection<ComprobanteDTO> comprobantes = new ArrayList<>();
+		consultarComprobanteDeAfiliadoAdapter.consultarTodosLosComprobantes(afiliadoDTO).forEach(comprobante->comprobantes.add(comprobante));
+		if(comprobantes.isEmpty()) return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		return ResponseEntity.status(HttpStatus.OK).body(comprobantes);
+		}
+		catch (AfiliadoNoExisteException a) {
+			return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Afiliado no existe");
+		}
+		catch (NumeroAfiliadoIncorrectoException | AfiliadoSinTitularException | 
+				DniConPuntosException | AfiliadoSinPlanException | PlanIncompletoException | 
+				PersonaIncompletaException e) {
+			return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Datos de afiliado incorrectos");
+		}
+	}
+	
+	@RequestMapping(value = "comprobantes/afiliado/numeroafiliado/{nroAfiliado}/fechadesde/{fechaDesde}/fechahasta/{fechaHasta}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public ResponseEntity<?> consultarTodosLosComprobantesPorFechas(@PathVariable("nroAfiliado")String nroAfiliado, 
+			@PathVariable("fechaDesde") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fechaDesde, 
+			@PathVariable("fechaHasta") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fechaHasta){
+		try {
+		AfiliadoDTO afiliadoDTO = consultarAfiliadoAdapter.consultarAfiliadoPorNumero(nroAfiliado);
+		Collection<ComprobanteDTO> comprobantes = new ArrayList<>();
+		consultarComprobanteDeAfiliadoAdapter.consultarComprobantesAfiliadoPorFechas(afiliadoDTO, fechaDesde, fechaHasta).forEach(comprobante->comprobantes.add(comprobante));
+		if(comprobantes.isEmpty()) return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		return ResponseEntity.status(HttpStatus.OK).body(comprobantes);
+		}
+		catch (AfiliadoNoExisteException a) {
+			return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Afiliado no existe");
+		}
+		catch (FechaIncorrectaException e) {
+			return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Fecha incorrecta");
+		}
+		catch (NumeroAfiliadoIncorrectoException | AfiliadoSinTitularException | 
+				DniConPuntosException | AfiliadoSinPlanException | PlanIncompletoException | 
+				PersonaIncompletaException e) {
+			return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED).body("Datos de afiliado incorrectos");
+		}
+	}
+	
+}
---
+++ Sermed-Boundaries/src/test/java/ar/com/koodi/sermedboundaries/ServiceIntegrationTest/ConsultarComprobanteDeAfiliadoIntegrationTest.java
@@ -0,0 +1,126 @@
+package ar.com.koodi.sermedboundaries.ServiceIntegrationTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
+import ModeloApi.ComprobanteDTO;
+
+public class ConsultarComprobanteDeAfiliadoIntegrationTest {
+
+	private String url = "http://localhost:8080";
+
+	@Test
+	public void consultarTodosLosComprobantes_ExistenComprobantes_Devuelve200() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url + "/sermed/comprobantes/afiliado/numeroafiliado/190001");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+	}
+
+	@Test
+	public void consultarTodosLosComprobantes_NoExistenComprobantes_Devuelve204() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url + "/sermed/comprobantes/afiliado/numeroafiliado/130000");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_NO_CONTENT));
+	}
+
+	@Test
+	public void consultarTodosLosComprobantes_NoExisteAfiliado_Devuelve412() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url + "/sermed/comprobantes/afiliado/numeroafiliado/000000");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_PRECONDITION_FAILED));
+	}
+
+	@Test
+	public void consultarTodosLosComprobantes_ExistenComprobantes_DevuelveJsonCorrecto() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url + "/sermed/comprobantes/afiliado/numeroafiliado/190001");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		String response = EntityUtils.toString(httpResponse.getEntity());
+		ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+				false);
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.registerModule(new ParameterNamesModule());
+		objectMapper.registerModule(new Jdk8Module());
+		ComprobanteDTO[] comprobanteDTOS = objectMapper.readValue(response, ComprobanteDTO[].class);
+		assertNotNull(comprobanteDTOS[0].idComprobante);
+	}
+
+	@Test
+	public void consultarComprobantesAfiliadoPorFechas_ExistenComprobantes_Devuelve200() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url
+				+ "/sermed/comprobantes/afiliado/numeroafiliado/190001/fechadesde/2018-10-01/fechahasta/2018-10-31");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+	}
+
+	@Test
+	public void consultarComprobantesAfiliadoPorFechas_NoExistenComprobantes_Devuelve204() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url
+				+ "/sermed/comprobantes/afiliado/numeroafiliado/190001/fechadesde/2018-01-01/fechahasta/2018-01-31");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_NO_CONTENT));
+	}
+
+	@Test
+	public void consultarComprobantesAfiliadoPorFechas_NoExisteAfiliado_Devuelve412() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url
+				+ "/sermed/comprobantes/afiliado/numeroafiliado/000000/fechadesde/2018-10-01/fechahasta/2018-10-31");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_PRECONDITION_FAILED));
+	}
+
+	@Test
+	public void consultarComprobantesAfiliadoPorFechas_ExistenComprobantes_DevuelveJsonCorrecto() throws Exception {
+		String token = TokenAuthentication.obtainAccessToken("usuario", "123456");
+		Header header = new BasicHeader("Authorization", "Bearer " + token);
+		HttpUriRequest request = new HttpGet(url
+				+ "/sermed/comprobantes/afiliado/numeroafiliado/190001/fechadesde/2018-10-01/fechahasta/2018-10-31");
+		request.setHeader(header);
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		String response = EntityUtils.toString(httpResponse.getEntity());
+		ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
+				false);
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.registerModule(new ParameterNamesModule());
+		objectMapper.registerModule(new Jdk8Module());
+		ComprobanteDTO[] comprobanteDTOS = objectMapper.readValue(response, ComprobanteDTO[].class);
+		assertNotNull(comprobanteDTOS[0].idComprobante);
+	}
+}
